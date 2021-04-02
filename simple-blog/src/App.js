4adbf4247034614736891ea21a8dbcc354e3b70f
Georgia Hoost <gfroehling@icloud.com>
2021-04-02 15:07:34 +0200

    pages back

--- simple-blog/src/App.js
+++ simple-blog/src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import
+{
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
 
-function App() {
+import Header from "./Components/Header";
+import Home from "./Components/Home";
+import Blog from "./Components/Blog";
+import Contact from "./Components/Contact";
+
+export default function App()
+{
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    <Router>
+      <div className="App">
+        <Header></Header>
+        <Switch>
+
+          <Route exact path="/"><Home /></Route>
+          <Route path="/blog"><Blog /></Route>
+          <Route path="/contact"><Contact /></Route>
+
+        </Switch>
+      </div>
+    </Router>
   );
 }
-
-export default App;
