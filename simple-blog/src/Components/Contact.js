118ad8e5cf61347f506efb4cd6e4097f7a2aa4a4
Georgia Hoost <gfroehling@icloud.com>
2021-04-08 18:48:22 +0200

    contact form styling

--- simple-blog/src/Components/Contact.js
+++ simple-blog/src/Components/Contact.js
@@ -2,8 +2,16 @@ const Contact = (props) =>
 {
     return (
 
-        <div>
-
+        <div className="contactContainer">
+            <h3>Contact</h3>
+            <p>Contact me, contact you.. ahaa, there is nothing we can do...</p>
+            <form>
+                <input type="text" value="Name" placeholder="Name"></input>
+                <input type="email" value="Email"></input>
+                <input type="text" value="Phone"></input>
+                <textarea type="message" value="Message"></textarea>
+                <button className="contactButton">Send message</button>
+            </form>
         </div>
     );
 }
